d1cb00a39231930c278ff1c21413128099fc2046
Taha Eghtesad <tahaeghtesad@gmail.com>
2021-10-07 15:19:12 -0500

    Add Sabine run script

---
+++ train_on_crowdhuman.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:8
+#SBATCH --mem=250GB
+#SBATCH -N 1
+
+
+source /project/cacds/apps/anaconda3/5.0.1/etc/profile.d/conda.sh
+
+conda activate mmdetection
+
+cd /home/teghtesa/carta-image-processing
+
+python mmdetection/tools/train.py new_yolof_config.py
