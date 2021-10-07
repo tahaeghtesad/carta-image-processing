2ac47bb3b3561a0b92fb4876b728c73a804e703a
Taha Eghtesad <tahaeghtesad@gmail.com>
2021-10-07 14:40:15 -0500

    update all files

--- new_yolof_config.py
+++ new_yolof_config.py
@@ -1,23 +1,27 @@
 _base_ = ['mmdetection/configs/yolof/yolof_r50_c5_8x8_1x_coco.py']
 data_root = 'crowdhuman/'
+classes = ('person', 'head', 'body')
+
 model = dict(
     roi_head=dict(
-        bbox_head=dict(num_classes=3),
-        mask_head=dict(num_classes=3)))
+        type='YOLOFHead',
+        bbox_head=dict(num_classes=len(classes)))
+)
 
 dataset_type = 'COCODataset'
-classes = ('person', 'head', 'body')
 
 data = dict(
     train=dict(
         ann_file=data_root + 'annotation_train.json',
         img_prefix=data_root + 'images_train/',
-        classes=classes
+        classes=classes,
+        type=dataset_type
     ),
     val=dict(
         ann_file=data_root + 'annotation_val.json',
         img_prefix=data_root + 'images_val/',
-        classes=classes
+        classes=classes,
+        type=dataset_type
     ),
     test=None,
 )
