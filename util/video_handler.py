bef9fb4aba73465e172add8b5e567b30d76435a5
Taha Eghtesad <tahaeghtesad@gmail.com>
2021-10-21 12:53:33 -0500

    fix a typo

--- util/video_handler.py
+++ util/video_handler.py
@@ -162,7 +162,7 @@ class VideoHandler:
             count += 1
 
         if not os.path.isdir(f'dataset/split/annotations'):
-            os.makedirs(f'dataset/split/annotation', exist_ok=True)
+            os.makedirs(f'dataset/split/annotations', exist_ok=True)
         util.file_handler.write_json(f'dataset/split/annotations/video_{id}.coco.json', dataset)
 
         # thread_pool.close()
