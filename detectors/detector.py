e40a0a29659bae172a4a8f743a356aca751581b3
Taha Eghtesad <tahaeghtesad@gmail.com>
2021-10-21 16:45:08 -0600

    typo

--- detectors/detector.py
+++ detectors/detector.py
@@ -2,7 +2,7 @@ class Detector:
     def __init__(self, config, checkpoint, detection_class, device='cuda:0') -> None:
         super().__init__()
         self.config = config
-        self.checkpoint = checkpoint,
+        self.checkpoint = checkpoint
         self.detection_class = detection_class
         self.device = device
 
