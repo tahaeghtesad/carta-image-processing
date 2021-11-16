ca923838fc47c3cac3a3e41f62bb112eb542d2ba
Taha Eghtesad <tahaeghtesad@gmail.com>
2021-11-16 11:51:22 -0600

    update crowdhuman config

--- configs/yolof_crowdhuman.py
+++ configs/yolof_crowdhuman.py
@@ -14,7 +14,7 @@ model = dict(
 dataset_type = 'CocoDataset'
 
 runner = dict(
-    max_epochs=12  # From base 12
+    max_epochs=24  # From base 12
 )
 
 img_norm_cfg = dict(
@@ -48,6 +48,8 @@ test_pipeline = [
 ]
 
 data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=1,
     train=dict(
         ann_file=[crowdhuman_data_root + 'annotation_train.json'],
         img_prefix=[crowdhuman_data_root + 'images_train/'],
@@ -71,4 +73,4 @@ data = dict(
     )
 )
 
-# load_from = 'checkpoints/yolof_r50_c5_8x8_1x_coco_20210425_024427-8e864411.pth'
+load_from = 'checkpoints/yolof_r50_c5_8x8_1x_coco_20210425_024427-8e864411.pth'
