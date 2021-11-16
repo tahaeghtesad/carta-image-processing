30232871f0b17d511b3bf4521fc2ec25505e25b3
Taha Eghtesad <tahaeghtesad@gmail.com>
2021-11-16 11:53:14 -0600

    update train script

--- train_mmdetection_slurm.sh
+++ train_mmdetection_slurm.sh
@@ -7,6 +7,8 @@
 #SBATCH -t 96:00:00
 
 echo "Running training for $1"
+echo "List of configs"
+echo "${@:2}"
 
 source /project/cacds/apps/anaconda3/5.0.1/etc/profile.d/conda.sh
 
@@ -14,4 +16,4 @@ conda activate mmdetection
 
 cd /home/teghtesa/carta-image-processing || exit 255
 
-bash mmdetection/tools/dist_train.sh "$1" 4
+bash mmdetection/tools/dist_train.sh "$1" 4 "${@:2}"
