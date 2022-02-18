ec6e37989a054d00737c61326160bd4f932ce88a
Taha Eghtesad <tahaeghtesad@gmail.com>
2022-02-18 14:22:14 -0600

    Severity must be an integer in [1, 5]

--- configs/faster_rcnn_cartai.py
+++ configs/faster_rcnn_cartai.py
@@ -27,7 +27,7 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(854, 480), keep_ratio=True),
-    dict(type='Corrupt', corruption='gaussian_noise', severity=.5),
+    dict(type='Corrupt', corruption='gaussian_noise', severity=1),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='RandomShift', shift_ratio=1, max_shift_px=16),
     dict(type='Normalize', **img_norm_cfg),
