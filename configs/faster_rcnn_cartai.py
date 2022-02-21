7397cf316665513f53d9ecceea92a1d0385e608e
Taha Eghtesad <tahaeghtesad@gmail.com>
2022-02-21 10:10:39 -0600

    update config

--- configs/faster_rcnn_cartai.py
+++ configs/faster_rcnn_cartai.py
@@ -32,7 +32,7 @@ train_pipeline = [
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='RandomShift', shift_ratio=0.5, max_shift_px=32),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Corrupt', corruption='gaussian_noise', severity=1),
+    # dict(type='Corrupt', corruption='gaussian_noise', severity=1), #Gausian noise makes the loss to become 'nan'
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
